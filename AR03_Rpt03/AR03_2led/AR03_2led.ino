812020b19a075e849fbcc4a1162ced5e467d20d4
GubyeongJun <48310065+GubyeongJun@users.noreply.github.com>
2019-10-10 16:35:11 +0900

    Update AR03_2led.ino

--- AR03_Rpt03/AR03_2led/AR03_2led.ino
+++ AR03_Rpt03/AR03_2led/AR03_2led.ino
@@ -5,8 +5,6 @@
 
 const int ledA   =  1;
 const int ledB   =  2;
-const int ledC   =  4;
-const int ledD   =  8;
 
 int number = 1;
 boolean flag = true;
@@ -16,31 +14,16 @@ void setup()
   Serial.begin(9600);
   pinMode(ledA, OUTPUT);
   pinMode(ledB, OUTPUT);
-  pinMode(ledC, OUTPUT);
-  pinMode(ledD, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(ledA,HIGH);
   digitalWrite(ledB,LOW);
-  digitalWrite(ledC,LOW);
-  digitalWrite(ledD,LOW);
   delay(100 * number);
   digitalWrite(ledA,LOW);
   digitalWrite(ledB,HIGH);
-  digitalWrite(ledC,LOW);
-  digitalWrite(ledD,LOW);
   delay(100 * number);
-  digitalWrite(ledA,LOW);
-  digitalWrite(ledB,LOW);
-  digitalWrite(ledC,HIGH);
-  digitalWrite(ledD,LOW);
-  delay(100 * number);
-  digitalWrite(ledA,LOW);
-  digitalWrite(ledB,LOW);
-  digitalWrite(ledC,LOW);
-  digitalWrite(ledD,HIGH);
   Serial.print("delay = ");
   Serial.print(100 * number);
   Serial.println(" msec");
