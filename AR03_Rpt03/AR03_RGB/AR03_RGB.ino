a924b3de4606b95f5b57a33c1145554d02cfe3be
GubyeongJun <48310065+GubyeongJun@users.noreply.github.com>
2019-10-10 16:33:37 +0900

    Update AR03_RGB.ino

--- AR03_Rpt03/AR03_RGB/AR03_RGB.ino
+++ AR03_Rpt03/AR03_RGB/AR03_RGB.ino
@@ -9,14 +9,40 @@ const int BlueLed   =  6;	  //LED B를 5번핀에 연결
 
 void setup()
 {
-  
-  ledOutput(255, 255, 0);
- 
+  ledOutput(255, 0, 0);
+  delay(1000);
+  ledOutput(0, 255, 0);
+  delay(1000);
+  ledOutput(0, 0, 255);
+  delay(1000);
 }
 
 void loop()
 {
-  
+  for(int i=0;i<=255;++i){
+    ledOutput(255,i,0);
+    delay(10);
+  }
+  for(int i=0;i<=255;++i){
+    ledOutput(0,255,i);
+    delay(10);
+  }
+  for(int i=0;i<=255;++i){
+    ledOutput(i,0,255);
+    delay(10);
+  }
+  for(int i=0;i<=255;++i){
+    ledOutput(i,255,255);
+    delay(10);
+  }
+  for(int i=0;i<=255;++i){
+    ledOutput(255,i,255);
+    delay(10);
+  }
+  for(int i=0;i<=255;++i){
+    ledOutput(255,255,i);
+    delay(10);
+  }
 }
 
 void ledOutput(int Red, int Green, int Blue){
@@ -24,5 +50,6 @@ void ledOutput(int Red, int Green, int Blue){
   analogWrite(GreenLed,Green);
   analogWrite(BlueLed,Blue);
 }
+ 
  
 
