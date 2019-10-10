57fc18b5d288ce5bbef9489884f31723071caf80
GubyeongJun <48310065+GubyeongJun@users.noreply.github.com>
2019-10-10 16:26:33 +0900

    Add files via upload

---
+++ AR03_Rpt03/AR03_RGB/AR03_RGB.ino
@@ -0,0 +1,28 @@
+/*
+ 예제 4.3
+ RGB-LED 밝기 조절
+*/
+
+const int RedLed    =  3;	  //를 3번핀에 연결
+const int GreenLed  =  5;	  //LED B를 5번핀에 연결
+const int BlueLed   =  6;	  //LED B를 5번핀에 연결
+
+void setup()
+{
+  
+  ledOutput(255, 255, 0);
+ 
+}
+
+void loop()
+{
+  
+}
+
+void ledOutput(int Red, int Green, int Blue){
+  analogWrite(RedLed,Red);
+  analogWrite(GreenLed,Green);
+  analogWrite(BlueLed,Blue);
+}
+ 
+
